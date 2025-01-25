1d1b949ea350abd6ff8d15fc4f17d96ea7d6cedf
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2025-01-24 19:41:41 -0500

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import Image from "next/image";
-import { Button } from "./components/Button";
+import { Button } from "./components/ui/Button";
+import { Slider } from "./components/ui/slider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <Button variant="default" size="lg">
-        Click me!
-      </Button>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col gap-4 w-full max-w-md">
+        {/* Button Component */}
+        <Button variant="default">Click me</Button>
+
+        {/* Slider Component */}
+        <div className="w-full">
+          <Slider defaultValue={[50]} max={100} step={1} />
+        </div>
+      </div>
     </main>
   );
 }
